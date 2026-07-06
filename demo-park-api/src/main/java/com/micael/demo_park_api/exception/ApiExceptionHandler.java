fafb1edfa2af8f43b74161497bbfe09824fa09d9
Micael Cruz Batista Martins <micaelcruz46@gmail.com>
2026-07-06 14:16:01 -0300

    feat: adiciona o método handleHttpRequestMethodNotSupportedException

--- demo-park-api/src/main/java/com/micael/demo_park_api/exception/ApiExceptionHandler.java
+++ demo-park-api/src/main/java/com/micael/demo_park_api/exception/ApiExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,7 +17,7 @@ public class ApiExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorMessage> methodArgumentNotValidException
-        (MethodArgumentNotValidException ex, HttpServletRequest request, BindingResult result){
+        (MethodArgumentNotValidException ex, HttpServletRequest request){
 
         return ResponseEntity
             .status(HttpStatus.UNPROCESSABLE_CONTENT)
@@ -24,6 +25,21 @@ public class ApiExceptionHandler {
             .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_CONTENT, "Campo(s) inválidos!"));
 
 
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorMessage> handleHttpRequestMethodNotSupportedException(
+        HttpRequestMethodNotSupportedException ex,
+        HttpServletRequest request) {
+
+        return ResponseEntity
+            .status(HttpStatus.METHOD_NOT_ALLOWED)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(new ErrorMessage(
+                request,
+                HttpStatus.METHOD_NOT_ALLOWED,
+                "Método HTTP não suportado para esta rota"
+            ));
     }
 
 
