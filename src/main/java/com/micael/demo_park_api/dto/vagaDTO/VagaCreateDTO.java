d607a4df88c99ff6c7e8141e99802406aa4a588a
Micael Cruz Batista Martins <micaelcruz46@gmail.com>
2026-08-27 18:08:05 -0300

    feat: adiciona base de internacionalizacao das mensagens de validacao
    - cria bundles de mensagens: pt-BR (padrao), en e es
    - VagaCreateDTO passa a referenciar chaves de mensagem no lugar de texto fixo

--- src/main/java/com/micael/demo_park_api/dto/vagaDTO/VagaCreateDTO.java
+++ src/main/java/com/micael/demo_park_api/dto/vagaDTO/VagaCreateDTO.java
@@ -3,13 +3,12 @@ package com.micael.demo_park_api.dto.vagaDTO;
 import com.micael.demo_park_api.domain.Vaga;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record VagaCreateDTO(
-    @NotBlank
-    @Size(min = 4, max = 4)
+    @NotBlank(message = "{NotBlank.VagaCreateDTO.codigoVaga}")
+    @Size(min = 4, max = 4, message = "{Size.VagaCreateDTO.codigoVaga}")
     String codigoVaga,
-    @NotNull
+    @NotNull(message = "{NotNull.VagaCreateDTO.statusVaga}")
     Vaga.StatusVaga statusVaga
 ){}
